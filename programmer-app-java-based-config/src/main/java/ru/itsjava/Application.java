db1751ef18cde02cf4a2250f01359000936e05a8
auskov77 <auskov77@yandex.ru>
2022-04-15 00:33:13 +0400

    programmer-app-java-based-config

--- programmer-app-java-based-config/src/main/java/ru/itsjava/Application.java
+++ programmer-app-java-based-config/src/main/java/ru/itsjava/Application.java
@@ -2,12 +2,9 @@ package ru.itsjava;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import ru.itsjava.services.ProgrammerService;
 
-@ComponentScan("ru.itsjava.configuration")
-@PropertySource("classpath:app.properties")
+
 public class  Application {
 
     public static void main(String[] args) {
